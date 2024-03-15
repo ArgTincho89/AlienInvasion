bdd80c3faa032b0020ee54ecf5f17ffd3472ad73
Martin <martin@coara.co>
2024-03-15 12:38:45 -0300

    Hability to shoot bullets added

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,8 @@
 import sys
-
 import pygame
-
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Clase general para gestionar los recursos y el comportamiento del juego."""
@@ -17,6 +16,7 @@ class AlienInvasion:
          (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")   
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
         
     def run_game(self):
@@ -24,6 +24,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()        
             self._update_screen()
             self.clock.tick(60)                            
                                                  
@@ -45,6 +46,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
             
     def _check_keyup_events(self, event):
         """Responde a liberaciones de teclas"""
@@ -53,13 +56,28 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
             
-                    
+    def _fire_bullet(self):
+        """Crea una neuva bala y la añade al grupo de balas."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
            
-                
+    def _update_bullets(self):
+        """Actualiza la posición de las balas y se deshace de las viejas."""
+        #Actualiza las posicions de las balas.
+        self.bullets.update()
+        
+        #Se deshace de las balas que han desaparecido.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+    
                 
     def _update_screen(self):
         """Actualiza las imágenes en la pantalla y cambia a la pantalla nueva"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
             
         #Hace visible la última pantalla dibujada.
--- settings.py
+++ settings.py
@@ -11,3 +11,10 @@ class Settings:
         
         #Configuración de la nave.
         self.ship_speed = 1.5
+        
+        #Configuración de las balas.
+        self.bullet_speed = 2.0
+        self.bullet_widht = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
