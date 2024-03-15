f1dd1708699fe5cd414890828ecba2aade085667
Martin <martin@coara.co>
2024-03-15 10:47:04 -0300

    refactorizacion general

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,16 +33,29 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-                    
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+                   
+    def _check_keydown_events(self, event):
+        """Responde a pulsaciones de teclas"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+            
+    def _check_keyup_events(self, event):
+        """Responde a liberaciones de teclas"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+            
+                    
+           
+                
                 
     def _update_screen(self):
         """Actualiza las imágenes en la pantalla y cambia a la pantalla nueva"""
